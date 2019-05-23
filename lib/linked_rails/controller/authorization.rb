6c31908c6e27f0f05ceb14d54c01de241d63fd6f
Arthur Dingemans <arthur@argu.co>
2019-05-23 12:06:30 +0200

    Authorize index_collection in index

--- lib/linked_rails/controller/authorization.rb
+++ lib/linked_rails/controller/authorization.rb
@@ -9,7 +9,11 @@ module LinkedRails
       end
 
       def authorize_action
-        authorize current_resource, "#{params[:action].chomp('!')}?"
+        if action_name == 'index'
+          authorize index_collection, :show?
+        else
+          authorize current_resource, "#{params[:action].chomp('!')}?"
+        end
       end
     end
   end
