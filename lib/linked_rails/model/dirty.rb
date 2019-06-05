a88defcf9a3103c8e31836af6a9cdef55a0a48b8
Arthur Dingemans <arthur@argu.co>
2019-06-05 15:30:48 +0200

    Keep track of destructed members to determine if an association is dirty

--- lib/linked_rails/model/dirty.rb
+++ lib/linked_rails/model/dirty.rb
@@ -3,13 +3,22 @@
 module LinkedRails
   module Model
     module Dirty
-      def association_changed?(association)
-        return false unless try(:association_cached?, association)
+      extend ActiveSupport::Concern
 
-        if self.class.reflect_on_association(association).collection?
-          send(association).any? { |a| a.previous_changes.present? }
-        else
-          send(association)&.previous_changes&.present?
+      included do
+        def assign_nested_attributes_for_one_to_one_association(association_name, attributes)
+          association_has_destructed(association_name) if will_destruct_association?(association_name, attributes)
+
+          super
+        end
+
+        def assign_nested_attributes_for_collection_association(association_name, attrs)
+          attributes = attrs
+          attributes = attrs.with_indifferent_access.key?(:id) ? [attrs] : attrs.values if attrs.is_a?(Hash)
+
+          association_has_destructed(association_name) if will_destruct_association?(association_name, attributes)
+
+          super
         end
       end
 
@@ -29,7 +38,39 @@ module LinkedRails
         return {} unless serializer_class
 
         serializer_class._reflections.select do |key, _value|
-          association_changed?(key.to_s.ends_with?('_collection') ? send(key).association : key)
+          association_key = key.to_s.ends_with?('_collection') ? send(key).association : key
+          association_has_destructed?(association_key) || association_changed?(association_key)
+        end
+      end
+
+      private
+
+      def association_changed?(association)
+        return false unless try(:association_cached?, association)
+
+        if self.class.reflect_on_association(association).collection?
+          send(association).any? { |a| a.previous_changes.present? }
+        else
+          send(association)&.previous_changes&.present?
+        end
+      end
+
+      def association_has_destructed(association_key)
+        @destructed_association_members ||= []
+        @destructed_association_members << association_key
+      end
+
+      def association_has_destructed?(association_key)
+        @destructed_association_members&.include?(association_key)
+      end
+
+      def will_destruct_association?(association_name, attributes)
+        return false unless nested_attributes_options[association_name][:allow_destroy]
+
+        if attributes.is_a?(Array)
+          attributes.any? { |attrs| has_destroy_flag?(attrs) }
+        else
+          has_destroy_flag?(attributes)
         end
       end
     end
