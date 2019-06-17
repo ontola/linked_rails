4b443ac223b0a3baaf9f863df8dfa9a640d7f73d
Arthur Dingemans <arthur@argu.co>
2019-06-17 16:56:26 +0200

    Handle ActiveRecord::RecordNotFound

--- lib/linked_rails/controller/error_handling.rb
+++ lib/linked_rails/controller/error_handling.rb
@@ -6,6 +6,7 @@ module LinkedRails
       extend ActiveSupport::Concern
       included do
         rescue_from StandardError, with: :handle_and_report_error
+        rescue_from ActiveRecord::RecordNotFound, with: :handle_error
         rescue_from Pundit::NotAuthorizedError, with: :handle_error
       end
 
