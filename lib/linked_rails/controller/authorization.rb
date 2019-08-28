f645a2d3b95ba3b3e0fcfc1f03ac81cc984d929f
Arthur Dingemans <arthur@argu.co>
2019-08-28 11:14:16 +0200

    Raise RecordNotFound when no authorization resource is present

--- lib/linked_rails/controller/authorization.rb
+++ lib/linked_rails/controller/authorization.rb
@@ -7,11 +7,11 @@ module LinkedRails
 
       def authorize_action # rubocop:disable Metrics/AbcSize
         if action_name == 'index'
-          raise('No collection present to authorize') if index_collection.blank?
+          raise ActiveRecord::RecordNotFound, 'No collection present to authorize' if index_collection.blank?
 
           authorize index_collection, :show?
         else
-          raise('No resource present to authorize') if current_resource.blank?
+          raise ActiveRecord::RecordNotFound, 'No resource present to authorize' if current_resource.blank?
 
           authorize current_resource, "#{params[:action].chomp('!')}?"
         end
