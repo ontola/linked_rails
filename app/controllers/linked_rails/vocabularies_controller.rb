c2065ed51c02ba89256e2440abde53c1f502df58
Arthur Dingemans <arthur@argu.co>
2019-09-23 13:08:28 +0200

    Refactor graph controller

--- app/controllers/linked_rails/vocabularies_controller.rb
+++ app/controllers/linked_rails/vocabularies_controller.rb
@@ -3,11 +3,12 @@
 module LinkedRails
   class VocabulariesController < ApplicationController
     active_response :show
+    attr_reader :graph
 
     private
 
     def add_class_data(klass, iri)
-      @graph << RDF::Statement.new(iri, RDF[:type], RDF::RDFS[:Class])
+      add_statement(RDF::Statement.new(iri, RDF[:type], RDF::RDFS[:Class]))
       add_subclasses(iri, klass)
       add_input_select_property(iri, klass)
       add_class_label(iri, klass)
@@ -20,7 +21,7 @@ module LinkedRails
         label = I18n.t("#{klass.name.tableize}.tooltips.info", default: nil, locale: locale)
         next if label.blank?
 
-        @graph << RDF::Statement.new(iri, NS::SCHEMA[:description], RDF::Literal.new(label, language: locale))
+        add_statement(RDF::Statement.new(iri, NS::SCHEMA[:description], RDF::Literal.new(label, language: locale)))
       end
     end
 
@@ -28,23 +29,23 @@ module LinkedRails
       I18n.available_locales.each do |locale|
         label = I18n.t("#{klass.name.tableize}.type", default: klass.name.underscore.humanize, locale: locale)
 
-        @graph << RDF::Statement.new(iri, RDF::RDFS[:label], RDF::Literal.new(label, language: locale))
+        add_statement(RDF::Statement.new(iri, RDF::RDFS[:label], RDF::Literal.new(label, language: locale)))
       end
     end
 
     def add_input_select_property(iri, klass)
-      @graph << RDF::Statement.new(iri, NS::ONTOLA['forms/inputs/select/displayProp'], klass.input_select_property)
+      add_statement(RDF::Statement.new(iri, NS::ONTOLA['forms/inputs/select/displayProp'], klass.input_select_property))
     end
 
     def add_property_icon(property_iri, icon)
       return if icon.blank?
 
-      @graph << RDF::Statement.new(property_iri, NS::SCHEMA[:image], RDF::URI("http://fontawesome.io/icon/#{icon}"))
+      add_statement(RDF::Statement.new(property_iri, NS::SCHEMA[:image], RDF::URI("http://fontawesome.io/icon/#{icon}")))
     end
 
     def add_property_data(klass)
       klass.predicate_mapping.each do |property_iri, value|
-        @graph << RDF::Statement.new(property_iri, RDF[:type], RDF[:Property])
+        add_statement(RDF::Statement.new(property_iri, RDF[:type], RDF[:Property]))
         add_property_label(property_iri, klass, value.name)
         add_property_icon(property_iri, value.options[:image])
       end
@@ -60,7 +61,7 @@ module LinkedRails
         end
         next if label.blank?
 
-        @graph << RDF::Statement.new(property_iri, RDF::RDFS[:label], RDF::Literal.new(label, language: locale))
+        add_statement(RDF::Statement.new(property_iri, RDF::RDFS[:label], RDF::Literal.new(label, language: locale)))
       end
     end
 
@@ -71,6 +72,10 @@ module LinkedRails
         .each { |opts| opts[:sh_in].each(&method(:dump_sh_in)) }
     end
 
+    def add_statement(statement)
+      graph << statement
+    end
+
     def add_subclasses(iri, klass)
       parent =
         if klass.superclass == ApplicationRecord
@@ -78,7 +83,7 @@ module LinkedRails
         else
           klass.superclass.iri
         end
-      @graph << RDF::Statement.new(iri, RDF::RDFS[:subClassOf], parent.is_a?(Array) ? parent.first : parent)
+      add_statement(RDF::Statement.new(iri, RDF::RDFS[:subClassOf], parent.is_a?(Array) ? parent.first : parent))
     end
 
     def authorize_action; end
@@ -88,7 +93,7 @@ module LinkedRails
         .new(option, adapter: :rdf, scope: user_context)
         .adapter
         .triples
-        .each { |t| @graph << t }
+        .each(&method(:add_statement))
     end
 
     def show_success
@@ -96,7 +101,7 @@ module LinkedRails
     end
 
     def vocab_graph
-      @graph = ::RDF::Graph.new
+      graph = ::RDF::Graph.new
       ApplicationRecord.descendants.each do |klass|
         iri = klass.iri.is_a?(Array) ? klass.iri.first : klass.iri
         add_class_data(klass, iri)
@@ -104,7 +109,7 @@ module LinkedRails
       LinkedRails::Form.descendants.each do |form|
         add_sh_in_options(form)
       end
-      @graph
+      graph
     end
   end
 end
