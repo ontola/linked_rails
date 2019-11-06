a230d83473f1ff58252379cc18c294263a67e82a
Arthur Dingemans <arthur@argu.co>
2019-11-06 14:36:56 +0100

    Make Form::Step#url callable

--- app/models/linked_rails/form/step.rb
+++ app/models/linked_rails/form/step.rb
@@ -3,10 +3,14 @@
 module LinkedRails
   class Form
     class Step < LinkedRails::SHACL::PropertyShape
-      attr_accessor :url
+      attr_writer :url
 
       def label
         @label.respond_to?(:call) ? form.instance_exec(&@label) : @label
+      end
+
+      def url
+        @url.respond_to?(:call) ? form.instance_exec(&@url) : @url
       end
 
       class << self
