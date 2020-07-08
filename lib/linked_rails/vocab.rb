52bbb8dbb7b6361072c3025890c6e6ce5d6c9bd1
Arthur Dingemans <arthur@argu.co>
2020-07-08 16:28:23 +0200

    Update vocabs

--- lib/linked_rails/vocab.rb
+++ lib/linked_rails/vocab.rb
@@ -4,8 +4,11 @@ require 'rdf'
 
 module LinkedRails
   module Vocab
+    FHIR = RDF::Vocabulary.new('http://hl7.org/fhir/')
+    FORM = RDF::Vocabulary.new('https://ns.ontola.io/form#')
+    LIBRO = RDF::Vocabulary.new('https://ns.ontola.io/libro/')
     LL = RDF::Vocabulary.new('http://purl.org/link-lib/')
-    ONTOLA = RDF::Vocabulary.new('https://ns.ontola.io/')
+    ONTOLA = RDF::Vocabulary.new('https://ns.ontola.io/core#')
     SP = RDF::Vocabulary.new('http://spinrdf.org/sp#')
   end
 end
