04165a814c075c14289dbbb8375ba48a37b99f65
Arthur Dingemans <arthur@argu.co>
2021-09-07 16:11:27 +0200

    Refactor enum value iri mapping

--- app/controllers/linked_rails/enum_values_controller.rb
+++ app/controllers/linked_rails/enum_values_controller.rb
@@ -3,47 +3,5 @@
 module LinkedRails
   class EnumValuesController < LinkedRails.controller_parent_class
     active_response :index
-
-    private
-
-    def enum_options!
-      serializer_class!.enum_options(params[:attribute]) || raise(ActiveRecord::RecordNotFound)
-    end
-
-    def requested_resource
-      return super unless action_name == 'index'
-
-      @requested_resource ||= LinkedRails::Sequence.new(
-        enum_options!.values,
-        id: index_iri,
-        scope: LinkedRails::EnumValuePolicy::Scope
-      )
-    end
-
-    def model_class
-      @model_class ||= self.class.linked_models.detect do |klass|
-        klass.to_s == ([params[:module]].compact + [params[:klass]&.singularize]).join('/').classify
-      end
-    end
-
-    def model_class!
-      model_class || raise(ActiveRecord::RecordNotFound)
-    end
-
-    def serializer_class
-      @serializer_class ||= RDF::Serializers.serializer_for(model_class!)
-    end
-
-    def serializer_class!
-      serializer_class || raise(ActiveRecord::RecordNotFound)
-    end
-
-    class << self
-      def linked_models
-        @linked_models ||= ObjectSpace.each_object(Class).select do |c|
-          c.included_modules.include?(LinkedRails::Model)
-        end
-      end
-    end
   end
 end
--- app/models/linked_rails/enum_value.rb
+++ app/models/linked_rails/enum_value.rb
@@ -29,5 +29,49 @@ module LinkedRails
     def label_from_variable
       @label.respond_to?(:call) ? @label.call : @label
     end
+
+    class << self
+      def class_for_params(params)
+        linked_models.detect do |klass|
+          klass.to_s == ([params[:module]].compact + [params[:klass]&.singularize]).join('/').classify
+        end
+      end
+
+      def enum_options(params)
+        serializer_for_params(params)&.enum_options(params[:attribute])
+      end
+
+      def linked_models
+        @linked_models ||= ObjectSpace.each_object(Class).select do |c|
+          c.included_modules.include?(LinkedRails::Model)
+        end
+      end
+
+      def requested_resource(opts, _user_context)
+        options = enum_options(opts[:params])
+
+        return unless options
+
+        LinkedRails::Sequence.new(
+          options.values,
+          id: sanitized_sequence_iri(opts[:iri]),
+          scope: LinkedRails::EnumValuePolicy::Scope
+        )
+      end
+
+      def sanitized_sequence_iri(raw_iri)
+        iri = RDF::URI(raw_iri)
+        iri.query = nil
+        iri.fragment = nil
+        iri.path = iri.path.split('.').first
+        iri
+      end
+
+      def serializer_for_params(params)
+        klass = class_for_params(params)
+
+        RDF::Serializers.serializer_for(klass) if klass
+      end
+    end
   end
 end
