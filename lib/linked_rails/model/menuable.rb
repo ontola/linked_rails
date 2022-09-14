3aeec22634f630da8dd8ebd4c2c2e33f54677a64
Arthur Dingemans <arthur@argu.co>
2022-09-14 12:05:03 +0200

    fix: handle missing menu list

--- lib/linked_rails/model/menuable.rb
+++ lib/linked_rails/model/menuable.rb
@@ -6,16 +6,16 @@ module LinkedRails
       extend ActiveSupport::Concern
 
       def menus(user_context = nil)
-        menu_list(user_context).menus
+        menu_list(user_context)&.menus
       end
 
       def menu(tag, user_context = nil)
-        menu_list(user_context).menu(tag)
+        menu_list(user_context)&.menu(tag)
       end
 
       def menu_list(user_context = nil)
         @menu_list ||= {}
-        @menu_list[user_context] ||= self.class.menu_class.new(resource: self, user_context: user_context)
+        @menu_list[user_context] ||= self.class.menu_class&.new(resource: self, user_context: user_context)
       end
 
       module ClassMethods
